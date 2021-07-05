81bc2eb2a3d780e46bd225e85bae43ff5501a2b2
Sergey <itsib.su@gmail.com>
2021-07-05 16:22:02 +0700

    Added native currency name

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -50,6 +50,16 @@ export const NETWORK_NAME: { [chainId in ChainId]: string } = {
   [ChainId.MUMBAI]: 'Polygon Matic Mumbai',
 };
 
+export const NATIVE_NAME: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: 'Ethereum',
+  [ChainId.RINKEBY]: 'Ethereum',
+  [ChainId.ROPSTEN]: 'Ethereum',
+  [ChainId.GÖRLI]: 'Ethereum',
+  [ChainId.KOVAN]: 'Ethereum',
+  [ChainId.MATIC]: 'Matic',
+  [ChainId.MUMBAI]: 'Matic',
+};
+
 export interface LpConfiguration {
   factory: string;
   router: string;
