e4b24d8fecfff705263a9eef4ad1d2b814c85fa7
Sergey <itsib.su@gmail.com>
2021-09-24 19:59:34 +0700

    release v2.4.0

--- src/types/zero-x/signed-0x-order-status.ts
+++ src/types/zero-x/signed-0x-order-status.ts
@@ -1,9 +1,7 @@
 export enum Signed0xOrderStatus {
-  Invalid,
-  InvalidMakerAssetAmount,
-  InvalidTakerAssetAmount,
-  Fillable,
-  Expired,
-  FullyFilled,
-  Cancelled,
+  INVALID,
+  FILLABLE,
+  FILLED,
+  CANCELLED,
+  EXPIRED,
 }
