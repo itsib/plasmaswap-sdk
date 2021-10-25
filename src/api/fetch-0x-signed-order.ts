30e9d7977db9b7fcc8dddababc256e5f271613d4
Sergey <itsib.su@gmail.com>
2021-10-26 01:55:39 +0700

    release v2.5.4

---
+++ src/api/fetch-0x-signed-order.ts
@@ -0,0 +1,23 @@
+import { ChainId } from '../constants/constants';
+import { Signed0xOrderWithMeta } from '../types';
+import { get0xApiUrl } from '../utils';
+
+export async function fetch0xSignedOrder(chainId: ChainId, hash: string, abort?: AbortSignal): Promise<Signed0xOrderWithMeta> {
+  const host = get0xApiUrl(chainId);
+  const url = `${host}/orderbook/v1/order/${hash}`;
+
+  const res: Response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    signal: abort,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error('Fetch order error, try again later.');
+  }
+
+  return await res.json();
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -3,4 +3,5 @@ export * from './fetch-pair-data';
 export * from './fetch-0x-quote';
 export * from './send-0x-signed-order';
 export * from './fetch-0x-signed-orders';
+export * from './fetch-0x-signed-order';
 export * from './fetch-0x-orderbook';
