786d422f3c00b0af6989fb43ee91f2b33216b02a
Sergey <itsib.su@gmail.com>
2021-10-26 19:03:58 +0700

    release v2.5.5

--- src/utils/format-hex-string.ts
+++ src/utils/format-hex-string.ts
@@ -1,4 +1,4 @@
-import { hexDataLength, hexDataSlice, hexValue, hexZeroPad } from '@ethersproject/bytes';
+import { hexDataLength, hexDataSlice, hexValue, hexZeroPad, isHexString } from '@ethersproject/bytes';
 
 const WORD_LENGTH = 32;
 
@@ -8,8 +8,11 @@ const WORD_LENGTH = 32;
  * @param length
  */
 export function formatHexString(value: string, length: number = WORD_LENGTH): string {
-  if (!value.startsWith('0x') && !isNaN(parseInt(value, 10))) {
-    value = hexValue(+value);
+  if (/^\d+$/.test(value)) {
+    value = hexValue(BigInt(value));
+  }
+  if (!isHexString(value)) {
+    throw new Error('Invalid value format');
   }
 
   return hexDataLength(value) < length ? hexZeroPad(value, length) : hexDataSlice(value, 0, length);
---
+++ test/format-hex-string.test.ts
@@ -0,0 +1,21 @@
+import { formatHexString } from '../src';
+
+describe('format-hex-string', () => {
+  it('String number value to hex', () => {
+    const amount = '232759384921936450232759384921936450';
+    const result = '0x00000000000000000000000000000000002cd3ec7725317f2946f18798311242';
+
+    expect(formatHexString(amount)).toEqual(result);
+  });
+
+  it('String value to hex', () => {
+    const value = '0x411Ab3796C0F78e2fba7ada86F4dFcebDcD7f27F';
+    const result = '0x000000000000000000000000411Ab3796C0F78e2fba7ada86F4dFcebDcD7f27F';
+    expect(formatHexString(value)).toEqual(result);
+  });
+
+  it('Invalid value', () => {
+    const amount = '23275938492193645023275938492193.6450';
+    expect(() => formatHexString(amount)).toThrowError('Invalid value format');
+  });
+});
